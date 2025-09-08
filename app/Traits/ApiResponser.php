222b5bc82be3eaacdf7ee8c3ba25d9fd182f786b
kd-leaker-190 <shakibzeidi0@gmail.com>
2025-09-08 09:51:12 +0330

    Api files

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+trait ApiResponser
+{
+    public function successResponse($data, $status, $message = '')
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data,
+        ], $status);
+    }
+    public function errorResponse($message, $status)
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => '',
+        ], $status);
+    }
+}
